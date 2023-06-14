a8fbf652a5f38e3a3e9244c0119f69cefa03acf1
19Edwin92 <b_a_s_e@naver.com>
2023-06-14 21:06:10 +0900

    디바운스 테스트

--- typescript/js.js
+++ typescript/js.js
@@ -11,30 +11,33 @@ const customDebounceUseCallback = customDebounce((text) => {
   }, 
   2000 
 )  
+
+customDebounceUseCallback("문자열");
+
   
 
   // customDebounceUseCallback를 보면 아래 함수가 반환된 값을 받기에 
-  let timeId = null;
-  const customDebounceUseCallback2 = (arg) => {
-    const closer = (callback, delay) => {
-      return (arg) => {
-        if (timeId) clearTimeout(timeId);
-        timeId = setTimeout(() => callback(arg), delay);
-      };
-    };
-    const customDebounceCallback = closer((text) => {
-      console.log(text);
-    }, 2000);
-    customDebounceCallback(arg);
-  };
+  // let timeId = null;
+  // const customDebounceUseCallback2 = (arg) => {
+  //   const closer = (callback, delay) => {
+  //     return (arg) => {
+  //       if (timeId) clearTimeout(timeId);
+  //       timeId = setTimeout(() => callback(arg), delay);
+  //     };
+  //   };
+  //   const customDebounceCallback = closer((text) => {
+  //     console.log(text);
+  //   }, 2000);
+  //   customDebounceCallback(arg);
+  // };
   
-  customDebounceUseCallback2("문자열");
+  // customDebounceUseCallback2("문자열");
   
 
 
 ////////////////////////////////////////////////////////////////////////////
 // 함수의 인수와 매개변수
-function getElement (parameter) { // 02 인수를 매개변수로 받음
-  console.log(parameter) // 03 매개변수를 출력
-}
+// function getElement (parameter) { // 02 인수를 매개변수로 받음
+//   console.log(parameter) // 03 매개변수를 출력
+// }
 // getElement("문자열") // 01 인수를 전달
