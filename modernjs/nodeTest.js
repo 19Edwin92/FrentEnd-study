bef977dac349c13065ac07bfe9c5cf4542ca9e62
19Edwin92 <b_a_s_e@naver.com>
2023-06-18 17:40:49 +0900

    모던자바스크립트 37. Set 정리중 (1)

--- modernjs/nodeTest.js
+++ modernjs/nodeTest.js
@@ -29,6 +29,9 @@ class ColorRectangle extends Rectangle {
 }
 
 const redRectangle = new ColorRectangle(10, 20, "red")
-console.log("redRectangle - ", redRectangle)
-console.log("redRectangle.getArea() - ", redRectangle.getArea())
-console.log("redRectangle.toString() - ", redRectangle.toString())
+// console.log("redRectangle - ", redRectangle)
+// console.log("redRectangle.getArea() - ", redRectangle.getArea())
+// console.log("redRectangle.toString() - ", redRectangle.toString())
+
+const uniq = array => array.filter((v, i, self) => self.indexOf(v) === i); 
+console.log(uniq([2,1,2,3,4,5,6]))
