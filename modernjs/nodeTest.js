9e6ca85a36bec5b5972765189c3a199466422baf
19Edwin92 <b_a_s_e@naver.com>
2023-06-18 20:25:50 +0900

    모던자바스크립트 37. Set 정리 끝(2), Map 정리 중(1)

--- modernjs/nodeTest.js
+++ modernjs/nodeTest.js
@@ -34,4 +34,20 @@ const redRectangle = new ColorRectangle(10, 20, "red")
 // console.log("redRectangle.toString() - ", redRectangle.toString())
 
 const uniq = array => array.filter((v, i, self) => self.indexOf(v) === i); 
-console.log(uniq([2,1,2,3,4,5,6]))
+// console.log(uniq([2,1,2,3,4,5,6]))
+
+// Set.prototype.difference = (set) => {
+//   const result = new Set([...this])
+//   for (const value of set) {
+//     result.delete(value)
+//   }
+//   return result 
+// }
+const set = (array) => {
+  return new Set([...array])
+}
+
+Set.prototype.difference = function (set) {
+  return new Set([...this].filter(v => !set.has(v)))
+}
+console.log(set([2,1,2,3,4,5,6]).difference(set([2,1,2,3,4,5])))
